c49b15f5d772a4237897e33de973cdae1e612328
Vishal Ambre <vambre21@gmail.com>
2020-02-08 22:04:46 +0530

    Title: Fix seconds to always show two digits

--- app/src/main/java/com/vishal/caffeine/extensionUtils.kt
+++ app/src/main/java/com/vishal/caffeine/extensionUtils.kt
@@ -1,7 +1,7 @@
 package com.vishal.caffeine
 
 fun Int.toMilliSeconds(): Long = (this * 60 * 1000).toLong()
-fun Long.toTime() : String = "${getMinutes(this)}:${getSeconds(this)}"
+fun Long.toTime() : String = "${getMinutes(this)}:${String.format("%02d",getSeconds(this))}"
 
 fun getMinutes(milliseconds : Long) : Long = (milliseconds)/(60 * 1000)
 fun getSeconds(milliseconds: Long) : Long = ((milliseconds) % (60 * 1000))/(1000)
